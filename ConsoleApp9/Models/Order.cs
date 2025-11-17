ccfda696f17a298489f4a04b68c8e457d6c33541
Дима Новиков <streamq1964@yandex.ru>
2025-11-17 09:53:02 +0300

    Создаие order и order item

---
+++ ConsoleApp9/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int PickupPointId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; }
+        public string PickupPointName { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ ConsoleApp9/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string ProductName { get; set; }
+    }
+}
