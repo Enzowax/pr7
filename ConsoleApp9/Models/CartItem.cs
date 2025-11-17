b9f4a72f305bfab8c95413a94e921561e65ed3db
Дима Новиков <streamq1964@yandex.ru>
2025-11-17 09:42:06 +0300

    Создание класса корзины

---
+++ ConsoleApp9/Models/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public decimal TotalPrice => ProductPrice * Quantity;
+    }
+}
